0b01deb0f7ed270ddc1bdbd392c04c37549c89b9
Chongkai Yu <44921794+PaPaQQQ@users.noreply.github.com>
2023-03-15 13:37:04 +0800

    Update vision.cpp

--- mask2former/modeling/pixel_decoder/ops/src/vision.cpp
+++ mask2former/modeling/pixel_decoder/ops/src/vision.cpp
@@ -14,7 +14,7 @@
 */
 
 #include "ms_deform_attn.h"
-
+# 函数封装
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("ms_deform_attn_forward", &ms_deform_attn_forward, "ms_deform_attn_forward");
   m.def("ms_deform_attn_backward", &ms_deform_attn_backward, "ms_deform_attn_backward");
